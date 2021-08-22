266182e91db26aa57e8d6b43d1a64a9cf9d651c9
Sergey Mironov <grrwlf@gmail.com>
2021-08-22 11:26:28 +0300

    Add nix-shell expressions

---
+++ default.nix
@@ -0,0 +1,33 @@
+{ pkgs ? import <nixpkgs> {}
+, stdenv ? pkgs.stdenv
+, git ? pkgs.git
+, ncurses ? pkgs.ncurses
+, which ? pkgs.which
+, utillinux ? pkgs.utillinux
+, makeWrapper ? pkgs.makeWrapper }:
+
+stdenv.mkDerivation {
+  name = "git-powercommit";
+  src = ./.;
+
+  checkInputs = [ git ncurses which utillinux ];
+  nativeBuildInputs = [ makeWrapper ];
+
+  doCheck = true;
+  checkPhase = ''
+    export GIT_USER="Test User"
+    export GIT_COMMITTER_EMAIL="nobody@nowhere.com"
+    export GIT_AUTHOR_EMAIL="nobody@nowhere.com"
+    $src/test.sh
+  '';
+
+  installPhase = ''
+    mkdir -pv $out/bin
+    cp $src/git-powercommit $out/bin
+    wrapProgram $out/bin/git-powercommit \
+      --prefix PATH : "${which}/bin" \
+      --prefix PATH : "${git}/bin" \
+      --prefix PATH : "${ncurses}/bin" \
+      --prefix PATH : "${utillinux}/bin" \
+  '';
+}
